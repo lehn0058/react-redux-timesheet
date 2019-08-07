f45a796122f0b180f7f9dc4c553ad1285db8a162
bclehner <bclehner@spscommerce.com>
2019-08-07 10:28:11 -0500

    lab 2 completed successfully

--- lab-02/src/App.js
+++ lab-02/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Projects from "./projects/Projects";
+import Projects from './projects/Projects';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Projects />
       </div>
     );
   }
